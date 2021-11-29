c8d79063960d9cceb4b42171c357f2e42d43e716
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-29 21:22:08 +0200

    userId-country

---
+++ src/database/models/savedList.ts
@@ -0,0 +1,41 @@
+import { Model, DataTypes} from "sequelize";
+
+import { sequelize } from "./index";
+/**
+ * User Class Model
+ */
+interface country {
+    region: string,
+    subRegion: string,
+    currencies: string,
+    languages: string
+
+}
+class SavedList extends Model {
+  userId!: string;
+
+  country!: country;
+
+  public readonly createdAt!: Date;
+
+  public readonly updatedAt!: Date;
+}
+
+SavedList.init(
+  {
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    tableName: `${process.env.DB_TABLE_PREFIX}SavedList`,
+    timestamps: true,
+  }
+);
+
+export default SavedList;
